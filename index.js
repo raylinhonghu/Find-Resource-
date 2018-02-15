b44ce8826582d02cb4c4e40418fd7ee278158ae4
raylinhonghu <raylinhonghu@gmail.com>
2018-02-14 18:50:13 -0800

    delete and error handler

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require('express');
 
 // set up express app
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+mongoose.Promise = global.Promise;
+
+app.use(bodyParser.json());
 
 // initialize routes
 app.use('/api',require('./routes/api'));
 
-app.get('/', function(req,res){
-  console.log("get request");
-  res.send({name:'Yoshi'});
-});
-
+// error handling middleware
+app.use(function(err,req,res,next){
+  res.status(422).send({error:err.message})
+})
 // listen for requests
 app.listen(process.env.PORT || 4000, function(){
   console.log("listening for requests ");
--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,32 @@
 const express = require('express');
-
 const router = express.Router();
-
+const Ninja = require('../models/ninjas');
 // get a list of ninjas from db
-router.get('/ninjas',function(req,res){
+router.get('/ninjas',function(req,res,next){
   res.send({type:'GET'});
+  console.log(req.body);
 });
 
 // add a new ninja
-router.post('/ninjas',function(req,res){
-  res.send({type:'POST'});
+router.post('/ninjas',function(req,res,next){
+  // equivalent to Ninja.create(req.body)
+  // var ninja = new Ninja(req.body);
+  // ninja.save();
+  Ninja.create(req.body).then(function(ninja){
+    res.send(ninja);
+  }).catch(next);
 });
 
 // update ninja
-router.put('/ninjas/:id',function(req,res){
+router.put('/ninjas/:id',function(req,res,next){
   res.send({type:'PUT'});
 });
 
 // delete ninja
-router.delete('/ninjas/:id',function(req,res){
-  res.send({type:'DELETE'});
+router.delete('/ninjas/:id',function(req,res,next){
+  Ninja.findByIdAndRemove({_id:req.params.id}).then(function(ninja){
+    res.send(ninja);
+  })
 });
 
 module.exports = router;
